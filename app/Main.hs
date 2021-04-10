b5f7bc4e2f9a541ae7b94ea7b4f8ed6cfeab9819
Christophe Thibaut <cthibaut@octo.com>
2021-04-10 11:23:27 +0200

    cleaner approach

--- app/Main.hs
+++ app/Main.hs
@@ -1,26 +1,58 @@
 {-# LANGUAGE OverloadedStrings #-}
 module Main where
 import Graphics.Blank
+import Data.Text (pack)
 
 {-
-   draw the pentagon, joining the vertices with lines
+   draw arcs joining every two vertices of the pentagon
 -}
 
-x0 = 300
-y0 = 300
-r = 100
-s = 2 * pi / 5
-pentagonVertices = [( x0 + r * cos (angle i)
-                    , y0 + r * sin (angle i) )
-                    |  i <- [0..4] ]
-    where angle n = n * s - pi / 2
+type Angle = Double
+type Coord = Double
+type Point = (Coord, Coord)
+unity = 200
+origin :: Point
+origin = (unity*2, unity*2)
 
-main = blankCanvas 3000 $ \ context -> do
-  send context $ do
-    mapM_ (\((x1,y1),(x2,y2)) -> do
-        moveTo (x1,y1)
-        lineTo (x2,y2)
-        )
-        $ zip pentagonVertices ((tail pentagonVertices) ++ [head pentagonVertices])
-    stroke ()
+coordX, coordY :: Point -> Coord
+coordX = fst
+coordY = snd
+
+distance :: Point -> Point -> Double
+distance (x0,y0) (x1,y1) = sqrt (dx * dx + dy * dy)
+    where
+        dx = x0 - x1
+        dy = y0 - y1
+
+r :: Double
+r = unity * 0.8
+
+pentagonAngles :: [Angle]
+pentagonAngles = take 5 (iterate (+ (2 * pi /5 )) (3 * pi / 2))
+startAngles    = map (+ (pi / 2  + pi / 5)) pentagonAngles
+pentagonVertices = [(x0 + r * cos a
+                    ,y0 + r * sin a)
+                      | a <- pentagonAngles]
+    where
+        x0 = coordX origin
+        y0 = coordY origin
+
+pentagonArcs :: [(Coord, Coord, Double, Angle, Angle, Bool)] 
+pentagonArcs = zipWith pentagonArc pentagonVertices startAngles
+    where
+        pentagonArc (xO,yO) angle = (xO, yO, edge, angle, angle + 3 * pi / 5, False)
+
+edge :: Double
+edge = distance (pentagonVertices !! 0) (pentagonVertices !! 1)
+
+main = do
+    blankCanvas 3000 $ \ context -> do
+        send context $ do
+            mapM_ (\args -> do
+                beginPath ()
+                arc args
+                stroke ()
+                closePath ()
+                  ) pentagonArcs
+
 
